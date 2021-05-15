2a72ba842e36c21731e70b2a512d9ac0f0bccf4e
Zachary Ranes <zachary.ranes@outlook.com>
2021-05-15 16:58:11 -0400

    Jumping added

--- Player/Player.gd
+++ Player/Player.gd
@@ -2,13 +2,31 @@ extends KinematicBody2D
 
 var motion = Vector2(0,0)
 
-const SPEED = 500
+const UP = Vector2(0,-1)
+const GRAVITY = 300
+const SPEED = 1000
+const JUMP_SPEED = 3000
 
 func _physics_process(delta):
+	apply_gravity()
+	jump()
+	move()
+	move_and_slide(motion, UP)
+
+func apply_gravity():
+	if is_on_floor():
+		motion.y = 0
+	else:
+		motion.y += GRAVITY
+		
+func jump():
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
+		motion.y -= JUMP_SPEED
+		
+func move():
 	if Input.is_action_pressed("Left") and not Input.is_action_pressed("Right"):
 		motion.x = -SPEED
 	elif Input.is_action_pressed("Right") and not Input.is_action_pressed("Left"):
 		motion.x = SPEED
 	else:
 		motion.x = 0
-	move_and_slide(motion)
